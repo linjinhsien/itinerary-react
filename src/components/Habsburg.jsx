eafc1d4374a12056caa276d968662bbc8ad14388
linjinhsien <linjinhsien@users.noreply.github.com>
2026-08-19 07:38:56 +0000

    嵌入哈布斯堡王朝行程地圖原圖

--- src/components/Habsburg.jsx
+++ src/components/Habsburg.jsx
@@ -1,4 +1,5 @@
 import { habsburgIntro, habsburgSections, habsburgSummary } from '../data/itinerary.js'
+import habsburgMap from '../assets/habsburg-map.jpg'
 
 export default function Habsburg() {
   return (
@@ -8,6 +9,17 @@ export default function Habsburg() {
         <h2>哈布斯堡王朝與這趟旅程</h2>
       </div>
       <div className="callout new">{habsburgIntro}</div>
+
+      <img
+        src={habsburgMap}
+        alt="哈布斯堡王朝與這趟旅程地圖：義大利文版歷史分析圖，標示米蘭的哈布斯堡統治、瑞士的哈布斯堡發源地與獨立戰爭、巴伐利亞的維特爾斯巴赫王朝與茜茜公主聯姻關係"
+        style={{
+          width: '100%',
+          borderRadius: '10px',
+          border: '1px solid var(--line)',
+          marginBottom: '18px',
+        }}
+      />
 
       {habsburgSections.map((item, i) => (
         <div className="faq-item" key={i}>
